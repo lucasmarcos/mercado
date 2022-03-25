cadca9f6610e3147013920e1f5a46963efc0625b
Lucas Marcos <lucasmarcos@live.com>
2022-03-25 15:25:39 -0300

    hmm

--- src/test/java/br/AppTest.java
+++ src/test/java/br/AppTest.java
@@ -1,8 +1,13 @@
 package br;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class AppTest 
 {
+    @Test
     public void shouldAnswerWithTrue()
     {
+	    assertEquals(true, true);
     }
 }
