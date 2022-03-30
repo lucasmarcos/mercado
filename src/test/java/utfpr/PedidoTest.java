d49f0daea3c78168c07d8dfa1e1c1d9214c17114
Lucas Marcos <lucasmarcos@users.noreply.github.com>
2022-03-29 21:14:45 -0300

    banana 10 reais 5% de juros

--- src/test/java/utfpr/PedidoTest.java
+++ src/test/java/utfpr/PedidoTest.java
@@ -7,11 +7,19 @@ public class PedidoTest {
 	@Test
 	public void pedidos() {
 		Pagamento pix = new Pagamento("Pix", 0.0);
-		Pedido pedido = new Pedido(pix);
-		Produto p1 = new Produto("Banana", 10.00);
+		Pagamento cartao = new Pagamento("Cartão", 5.0);
 
-		pedido.adicionarProduto(p1, 10);
+		Produto banana = new Produto("Banana", 10.00);
+
+		Pedido pedido = new Pedido(pix);
+
+		pedido.adicionarProduto(banana, 10);
 
 		assertEquals(pedido.valorTotal(), 100.00);
+
+		Pedido pedido2 = new Pedido(cartao);
+		pedido2.adicionarProduto(banana, 1);
+
+		assertEquals(pedido2.valorTotal(), 10.5);
 	}
 }
