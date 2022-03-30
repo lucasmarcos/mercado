2431688eca5fa17c0f554427968c15c2d1ce049b
Lucas Marcos <lucasmarcos@users.noreply.github.com>
2022-03-29 21:52:13 -0300

    testes separados

--- src/test/java/utfpr/EstoqueTest.java
+++ src/test/java/utfpr/EstoqueTest.java
@@ -1,36 +1,51 @@
 package utfpr;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EstoqueTest {
+	private Estoque estoque;
+	private Produto banana;
+
+	@BeforeEach
+	void setup() {
+		this.estoque = new Estoque();
+		this.banana = new Produto("Banana", 10.00);
+	}
+
 	@Test
-	void test() {
-		Estoque estoque = new Estoque();
-
-		Produto banana = new Produto("Banana", 10.00);
-
+	void qtdEmEstoque() {
 		estoque.adicionarProduto(banana, 124);
-
 		assertEquals(estoque.qtdEmEstoque(banana), 124);
+	}
 
+	@Test
+	void limiar() {
+		estoque.adicionarProduto(banana, 124);
 		assertTrue(estoque.conferirEstoque(banana, 123));
 		assertTrue(estoque.conferirEstoque(banana, 124));
 		assertFalse(estoque.conferirEstoque(banana, 125));
-
-		estoque.atualizarProduto(banana, 320);
-		assertEquals(estoque.qtdEmEstoque(banana), 320);
-
-		estoque.removerProduto(banana);
-		assertEquals(estoque.qtdEmEstoque(banana), 0);
-
-		estoque.adicionarProduto(banana, 10);
-		estoque.adicionarProduto(banana, 4);
-
-		assertEquals(estoque.qtdEmEstoque(banana), 14);
 	}
 
+	@Test
+	void atualizar() {
+		estoque.adicionarProduto(banana, 124);
+		estoque.atualizarProduto(banana, 320);
+		assertEquals(estoque.qtdEmEstoque(banana), 320);
+	}
+
+	@Test
+	void remover() {
+		estoque.adicionarProduto(banana, 124);
+		estoque.removerProduto(banana);
+		assertEquals(estoque.qtdEmEstoque(banana), 0);
+	}
+
+	@Test
+	void somar() {
+		estoque.adicionarProduto(banana, 10);
+		estoque.adicionarProduto(banana, 4);
+		assertEquals(estoque.qtdEmEstoque(banana), 14);
+	}
 }
-
-
